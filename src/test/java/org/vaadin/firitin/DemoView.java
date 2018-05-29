e30ee24a9d36138afdd9635fd0eaf2b6d5f38bae
Mirjan Merruko <mmerruko@gmail.com>
2018-05-29 14:32:57 +0300

    Add viritin test domain and improve the test view

--- src/test/java/org/vaadin/firitin/DemoView.java
+++ src/test/java/org/vaadin/firitin/DemoView.java
@@ -1,23 +1,40 @@
 package org.vaadin.firitin;
 
-import org.vaadin.firitin.button.VButton;
-import org.vaadin.firitin.button.VButton.ButtonColor;
-import org.vaadin.firitin.button.VButton.ButtonType;
+import org.vaadin.firitin.components.VButton;
+import org.vaadin.firitin.components.VButton.ButtonColor;
+import org.vaadin.firitin.components.VButton.ButtonSize;
+import org.vaadin.firitin.components.VButton.ButtonType;
+import org.vaadin.firitin.components.VCheckBox;
+import org.vaadin.firitin.components.VComboBox;
+import org.vaadin.firitin.testdomain.Person;
 
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.TemplateRenderer;
 import com.vaadin.flow.router.Route;
 
+import io.github.benas.randombeans.api.EnhancedRandom;
+
 @Route("")
-public class DemoView extends Div {
+public class DemoView extends VerticalLayout {
 
     public DemoView() {
-        VButton button = new VButton().withText("Click me").withColor(ButtonColor.SUCCESS).withType(ButtonType.PRIMARY)
-                .withClickListener(e -> Notification.show("Hello!"));
+        VButton button = new VButton().withAutofocus(true).withColor(ButtonColor.SUCCESS).withType(ButtonType.PRIMARY)
+                .withSize(ButtonSize.LARGE).withText("Click me!").withClickListener(e -> Notification.show("Clicked!"));
 
-        button.addBlurListener(e -> {
-           Notification.show("Blurred!"); 
-        });
         add(button);
+
+        VCheckBox checkbox = new VCheckBox().withValue(true)
+                .withValueChangeListener(v -> Notification.show("New Value : " + v.getValue()))
+                .withLabel("Checkbox Label");
+
+        add(checkbox);
+
+        VComboBox<Person> comboBox = new VComboBox<Person>().withItems(EnhancedRandom.randomListOf(100, Person.class))
+                .withAllowCustomValue(false).withLabel("Person").withItemLabelGenerator(Person::getLastName)
+                .withRenderer(TemplateRenderer.<Person>of("<span style=\"background:yellow\">[[item.name]]</span>")
+                        .withProperty("name", Person::getLastName))
+                .withValueChangeListener(v -> Notification.show("Selected " + v.getValue().getLastName()));
+        add(comboBox);
     }
 }
