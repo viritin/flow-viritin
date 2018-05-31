6a20e5d5a98a0dfb88778a5b502756f4aeae27bc
Stefan Freude <freude.stefan@guj.de>
2018-05-31 15:51:08 +0200

    VDiv with FluentHasComponents

--- src/main/java/org/vaadin/firitin/components/VDiv.java
+++ src/main/java/org/vaadin/firitin/components/VDiv.java
@@ -3,9 +3,10 @@ package org.vaadin.firitin.components;
 import com.vaadin.flow.component.Component;
 import org.vaadin.firitin.fluency.ui.FluentClickNotifier;
 import org.vaadin.firitin.fluency.ui.FluentComponent;
+import org.vaadin.firitin.fluency.ui.FluentHasComponents;
 
 public class VDiv extends GenericDiv
-        implements FluentComponent<VDiv>, FluentClickNotifier<GenericDiv, VDiv>
+        implements FluentComponent<VDiv>, FluentClickNotifier<GenericDiv, VDiv>, FluentHasComponents<VDiv>
 {
     public VDiv() {
         super();
