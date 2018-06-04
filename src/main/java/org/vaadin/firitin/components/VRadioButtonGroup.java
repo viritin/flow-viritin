d630b4cc9153c8223dcb2a98b3cea236904b4bf9
Stefan Freude <freude.stefan@guj.de>
2018-06-04 15:13:11 +0200

    Label and renderer for VRadioButtonGroup

--- src/main/java/org/vaadin/firitin/components/VRadioButtonGroup.java
+++ src/main/java/org/vaadin/firitin/components/VRadioButtonGroup.java
@@ -1,8 +1,11 @@
 package org.vaadin.firitin.components;
 
 import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
+import com.vaadin.flow.component.ItemLabelGenerator;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.data.renderer.TextRenderer;
 import org.vaadin.firitin.fluency.ui.*;
 import org.vaadin.firitin.fluency.ui.internal.FluentHasAutofocus;
 import org.vaadin.firitin.fluency.ui.internal.FluentHasLabel;
@@ -15,9 +18,19 @@ public class VRadioButtonGroup<T> extends RadioButtonGroup<T> implements FluentH
         super();
     }
 
+    public VRadioButtonGroup(String label) {
+        super();
+        add(new Label(label));
+    }
+
     @Override
-    public VRadioButtonGroup withId(String id) {
+    public VRadioButtonGroup<T> withId(String id) {
         setId(id);
+        return this;
+    }
+
+    public VRadioButtonGroup<T> withTextRenderer(ItemLabelGenerator<T> itemLabelGenerator) {
+        setRenderer(new TextRenderer<>(itemLabelGenerator));
         return this;
     }
 }
