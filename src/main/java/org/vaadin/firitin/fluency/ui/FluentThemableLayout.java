fe2eab372187174c69c8c7689a3bc23a244f984a
Mirjan Merruko <mmerruko@gmail.com>
2018-06-10 22:20:03 +0300

    Minor - add a suppress warning on the class itself

--- src/main/java/org/vaadin/firitin/fluency/ui/FluentThemableLayout.java
+++ src/main/java/org/vaadin/firitin/fluency/ui/FluentThemableLayout.java
@@ -3,27 +3,24 @@ package org.vaadin.firitin.fluency.ui;
 import com.vaadin.flow.component.orderedlayout.BoxSizing;
 import com.vaadin.flow.component.orderedlayout.ThemableLayout;
 
+@SuppressWarnings("unchecked")
 public interface FluentThemableLayout<S extends FluentThemableLayout<S>> extends ThemableLayout {
 
-    @SuppressWarnings("unchecked")
     default S withMargin(boolean margin) {
         setMargin(margin);
         return (S) this;
     }
 
-    @SuppressWarnings("unchecked")
     default S withPadding(boolean padding) {
         setPadding(padding);
         return (S) this;
     }
 
-    @SuppressWarnings("unchecked")
     default S withSpacing(boolean spacing) {
         setSpacing(spacing);
         return (S) this;
     }
 
-    @SuppressWarnings("unchecked")
     default S withBoxSizing(BoxSizing boxSizing) {
         setBoxSizing(boxSizing);
         return (S) this;
