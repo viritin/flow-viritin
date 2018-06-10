9c8896bbe53901125795b5531b9db74aec1e997b
Mirjan Merruko <mmerruko@gmail.com>
2018-06-10 22:19:34 +0300

    Change methods to be closer to V10 terminology and documentation

--- src/main/java/org/vaadin/firitin/fluency/ui/FluentHasStyle.java
+++ src/main/java/org/vaadin/firitin/fluency/ui/FluentHasStyle.java
@@ -5,72 +5,27 @@ import com.vaadin.flow.component.HasStyle;
 @SuppressWarnings("unchecked")
 public interface FluentHasStyle<S extends FluentHasStyle<S>> extends HasStyle {
 
-    // Javadoc copied form Vaadin Framework
-
     /**
-     * Changes the primary style name of the component.
-     *
-     * <p>
-     * The primary style name identifies the component when applying the CSS theme to the Component. By changing the
-     * style name all CSS rules targeted for that style name will no longer apply, and might result in the component not
-     * working as intended.
-     * </p>
+     * Sets the CSS class names of this component. This method overwrites any
+     * previous set class names.
      *
      * @param className
-     *            The new primary style name
-     * @return this (for method chaining)
-     * @see #setClassName(String)
+     *            a space-separated string of class names to set, or
+     *            <code>null</code> to remove all class names
      */
-    default S withPrimaryClassName(String className) {
+    default S withClassName(String className) {
         setClassName(className);
         return (S) this;
     }
 
-    // Javadoc copied form Vaadin Framework
-
     /**
-     * Sets one or more user-defined style names of the component, replacing any previous user-defined styles. Multiple
-     * styles can be specified as a space-separated list of style names. The style names must be valid CSS class names
-     * and should not conflict with any built-in style names in Vaadin or GWT.
-     *
-     * <pre>
-     * Label label = new Label(&quot;This text has a lot of style&quot;);
-     * label.setStyleName(&quot;myonestyle myotherstyle&quot;);
-     * </pre>
-     *
-     * <p>
-     * Each style name will occur in two versions: one as specified and one that is prefixed with the style name of the
-     * component. For example, if you have a {@code Button} component and give it "{@code mystyle}" style, the component
-     * will have both "{@code mystyle}" and "{@code v-button-mystyle}" styles. You could then style the component either
-     * with:
-     * </p>
-     *
-     * <pre>
-     * .myonestyle {background: blue;}
-     * </pre>
-     *
-     * <p>
-     * or
-     * </p>
-     *
-     * <pre>
-     * .v-button-myonestyle {background: blue;}
-     * </pre>
-     *
-     * <p>
-     * It is normally a good practice to use {@link #addClassName(String) addStyleName()} rather than this setter, as
-     * different software abstraction layers can then add their own styles without accidentally removing those defined
-     * in other layers.
-     * </p>
+     * Adds one or more CSS class names to this component. Multiple class names can
+     * be specified by using multiple parameters.
      *
      * @param classNames
-     *            the new style or styles of the component as a space-separated list
-     * @return this (for method chaining)
-     * @see #getClassName()
-     * @see #addClassName(String)
-     * @see #removeClassName(String)
+     *            the CSS class name or class names to be added to the component
      */
-    default S withClassName(String... classNames) {
+    default S withAddedClassName(String... classNames) {
         for (String className : classNames) {
             addClassName(className);
         }
