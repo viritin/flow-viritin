bf4cce0e6bfd7bc42a3d7c9513a581633514c18d
Matti Tahvonen <matti@vaadin.com>
2019-01-03 20:26:35 +0200

    Added test for BorderLayout and adjusted build to be bit simpler and compatible with html imports from the project

---
+++ src/test/java/org/vaadin/firitin/BorderLayoutExample.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 Viritin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.vaadin.firitin;
+
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.router.Route;
+import org.vaadin.firitin.layouts.BorderLayout;
+
+/**
+ *
+ * @author mstahv
+ */
+@Route
+public class BorderLayoutExample extends BorderLayout {
+
+    public BorderLayoutExample() {
+        setChildAt(Region.EAST, new Span("East"));
+        setChildAt(Region.NORTH, new Span("North"));
+        setChildAt(Region.WEST, new Span("West"));
+        setChildAt(Region.SOUTH, new Span("South"));
+        
+    }
+    
+}
