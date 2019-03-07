7381a4757ae35acaf66fc5bdd3e7d58b6715b3af
Marten Prieß <marten@rocketbase.io>
2019-03-07 11:17:08 +0100

    refactored existing html-components

--- src/main/java/org/vaadin/firitin/components/VDiv.java
+++ src/main/java/org/vaadin/firitin/components/VDiv.java
@@ -1,27 +1,14 @@
 package org.vaadin.firitin.components;
 
-import com.vaadin.flow.component.Text;
-import org.vaadin.firitin.fluency.ui.FluentClickNotifier;
-import org.vaadin.firitin.fluency.ui.FluentComponent;
-import org.vaadin.firitin.fluency.ui.FluentHasComponents;
-import org.vaadin.firitin.fluency.ui.FluentHasSize;
-import org.vaadin.firitin.fluency.ui.FluentHasStyle;
-import org.vaadin.firitin.fluency.ui.FluentThemableLayout;
-
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.HtmlContainer;
-import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Div;
+import org.vaadin.firitin.fluency.ui.FluentClickNotifierWithoutTypedSource;
+import org.vaadin.firitin.fluency.ui.FluentHtmlContainer;
 
-@Tag(Tag.DIV)
-public class VDiv extends HtmlContainer implements FluentComponent<VDiv>, FluentClickNotifier<VDiv, VDiv>,
-        FluentHasComponents<VDiv>, FluentHasStyle<VDiv>, FluentThemableLayout<VDiv>, FluentHasSize<VDiv> {
+public class VDiv extends Div implements FluentHtmlContainer<VDiv>, FluentClickNotifierWithoutTypedSource<VDiv> {
     public VDiv() {
         super();
-    }
-
-    public VDiv(String text) {
-        super();
-        add(new Text(text));
     }
 
     public VDiv(Component... components) {
--- src/main/java/org/vaadin/firitin/components/VH3.java
+++ src/main/java/org/vaadin/firitin/components/VH3.java
@@ -2,10 +2,10 @@ package org.vaadin.firitin.components;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.H3;
-import org.vaadin.firitin.fluency.ui.FluentHasComponents;
-import org.vaadin.firitin.fluency.ui.FluentHasStyle;
+import org.vaadin.firitin.fluency.ui.FluentClickNotifierWithoutTypedSource;
+import org.vaadin.firitin.fluency.ui.FluentHtmlContainer;
 
-public class VH3 extends H3 implements FluentHasComponents<VH3>, FluentHasStyle<VH3> {
+public class VH3 extends H3 implements FluentHtmlContainer<VH3>, FluentClickNotifierWithoutTypedSource<VH3> {
     public VH3() {
         super();
     }
