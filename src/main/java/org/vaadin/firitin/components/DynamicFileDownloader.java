20532cf53821ebe9f789c344ab060af1ae87d398
Matti Tahvonen <matti@vaadin.com>
2019-04-26 10:02:57 +0300

    fixed package

--- src/main/java/org/vaadin/firitin/components/DynamicFileDownloader.java
+++ src/main/java/org/vaadin/firitin/components/DynamicFileDownloader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.vaadin.firitin.components.html;
+package org.vaadin.firitin.components;
 
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.UI;
