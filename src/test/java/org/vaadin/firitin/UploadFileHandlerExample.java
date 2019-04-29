f259b3d90b066339e73ed91a579a2f688dbf4e69
Matti Tahvonen <matti@vaadin.com>
2019-04-29 14:05:30 +0300

    Added an alternative example to show contents of CSV file in a Grid

--- src/test/java/org/vaadin/firitin/UploadFileHandlerExample.java
+++ src/test/java/org/vaadin/firitin/UploadFileHandlerExample.java
@@ -15,11 +15,19 @@
  */
 package org.vaadin.firitin;
 
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.Route;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.vaadin.firitin.components.upload.UploadFileHandler;
@@ -29,27 +37,70 @@ import org.vaadin.firitin.components.upload.UploadFileHandler;
  * @author mstahv
  */
 @Route
+@Push
 public class UploadFileHandlerExample extends VerticalLayout {
+
+    private static final String DELIMITER = ";";
 
     public UploadFileHandlerExample() {
         UploadFileHandler uploadFileHandler = new UploadFileHandler(
                 (InputStream content, String fileName, String mimeType) -> {
-            try {
-                int b = 0;
-                int count = 0;
-                while ((b = content.read()) != -1) {
-                    if (b == "\n".getBytes()[0]) {
-                        count++;
+                    try {
+                        int b = 0;
+                        int count = 0;
+                        while ((b = content.read()) != -1) {
+                            if (b == "\n".getBytes()[0]) {
+                                count++;
+                            }
+                        }
+                        String msg = "Counted " + count + "lines";
+                        getUI().get().access(() -> Notification.show(msg));
+                    } catch (IOException ex) {
+                        Logger.getLogger(UploadFileHandlerExample.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                }
-                String msg = "Counted " + count + "lines";
-                getUI().get().access(() -> Notification.show(msg));
-            } catch (IOException ex) {
-                Logger.getLogger(UploadFileHandlerExample.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        });
-        
-        add(uploadFileHandler);
+                });
+
+        Grid<String[]> grid = new Grid<>();
+
+        UploadFileHandler csvFileHandler = new UploadFileHandler(
+                (InputStream content, String fileName, String mimeType) -> {
+                    try {
+
+                        BufferedReader br = new BufferedReader(new InputStreamReader(content));
+                        String line = br.readLine();
+
+                        getUI().get().access(() -> {
+
+                        });
+                        String[] headers = line.split(";");
+                        getUI().get().access(() -> {
+                            grid.removeAllColumns();
+                            // create headers from first row
+                            for (int i = 0; i < headers.length; i++) {
+                                int index = i;
+                                grid.addColumn(lineDataArray -> {
+                                    return lineDataArray[index];
+                                }).setHeader(headers[index]);
+                            }
+
+                        });
+
+                        // Collect data
+                        List<String[]> records = new ArrayList<>();
+                        while ((line = br.readLine()) != null) {
+                            String[] values = line.split(DELIMITER);
+                            records.add(values);
+                        }
+                        getUI().get().access(() -> {
+                            grid.setItems(records);
+                        });
+                    } catch (IOException ex) {
+                        Logger.getLogger(UploadFileHandlerExample.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                });
+
+        add(new Paragraph("Count lines"), uploadFileHandler,
+                new Paragraph("Display CSV file"), csvFileHandler, grid);
 
     }
 
