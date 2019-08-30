a2a81d5932a052ac150a806534d78b2904dc9e11
Matti Tahvonen <matti@vaadin.com>
2019-08-30 22:29:56 +0300

    VTextField is now lazy by default -> decent default for fluent forms

--- src/main/java/org/vaadin/firitin/components/textfield/VTextField.java
+++ src/main/java/org/vaadin/firitin/components/textfield/VTextField.java
@@ -12,6 +12,7 @@ import org.vaadin.firitin.fluency.ui.internal.FluentHasLabel;
 
 import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 
 public class VTextField extends TextField
         implements FluentHasSize<VTextField>, FluentHasValidation<VTextField>, FluentFocusable<TextField, VTextField>,
@@ -20,32 +21,50 @@ public class VTextField extends TextField
 
     public VTextField() {
         super();
+        initializeSettings();
+    }
+
+    protected void initializeSettings() {
+        // make validations work properly
+        setValueChangeMode(ValueChangeMode.LAZY);
     }
 
     public VTextField(String label) {
         super(label);
+        // make validations work properly
+        initializeSettings();
     }
 
     public VTextField(String label, String placeholder) {
         super(label, placeholder);
+        // make validations work properly
+        initializeSettings();
     }
 
     public VTextField(String label, String initialValue, String placeholder) {
         super(label, initialValue, placeholder);
+        // make validations work properly
+        initializeSettings();
     }
 
     public VTextField(ValueChangeListener<? super ComponentValueChangeEvent<TextField, String>> listener) {
         super(listener);
+        // make validations work properly
+        initializeSettings();
     }
 
     public VTextField(String label,
             ValueChangeListener<? super ComponentValueChangeEvent<TextField, String>> listener) {
         super(label, listener);
+        // make validations work properly
+        initializeSettings();
     }
 
     public VTextField(String label, String initialValue,
             ValueChangeListener<? super ComponentValueChangeEvent<TextField, String>> listener) {
         super(label, initialValue, listener);
+        // make validations work properly
+        initializeSettings();
     }
 
     public VTextField withRequired(boolean required) {
