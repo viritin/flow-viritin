22aa57936a1218bba870cfa645313741b1bf9334
Marten <marten@rocketbase.io>
2020-01-23 22:34:04 +0100

    added missing methods + added shorthands

--- src/main/java/org/vaadin/firitin/components/formlayout/VFormLayout.java
+++ src/main/java/org/vaadin/firitin/components/formlayout/VFormLayout.java
@@ -6,6 +6,8 @@ import org.vaadin.firitin.fluency.ui.FluentFlexComponent;
 import org.vaadin.firitin.fluency.ui.FluentHasComponents;
 import org.vaadin.firitin.fluency.ui.FluentThemableLayout;
 
+import java.util.List;
+
 public class VFormLayout extends FormLayout implements FluentThemableLayout<VFormLayout>,
         FluentFlexComponent<VFormLayout>, FluentHasComponents<VFormLayout> {
 
@@ -22,11 +24,52 @@ public class VFormLayout extends FormLayout implements FluentThemableLayout<VFor
      * you want full width component, give 2 as a last parameter.
      *
      * @param component the component
-     * @param label the label for component
-     * @param colspan the amount of columns this component should consume
+     * @param label     the label for component
+     * @param colspan   the amount of columns this component should consume
      */
     public void addFormItem(Component component, String label, int colspan) {
         FormItem formItem = addFormItem(component, label);
         formItem.getElement().setAttribute("colspan", "" + colspan);
+    }
+
+    public VFormLayout withFormItem(Component component, String label, int colspan) {
+        addFormItem(component, label, colspan);
+        return this;
+    }
+
+    public VFormLayout withResponsiveSteps(ResponsiveStep... steps) {
+        setResponsiveSteps(steps);
+        return this;
+    }
+
+    public VFormLayout withResponsiveSteps(List<ResponsiveStep> steps) {
+        setResponsiveSteps(steps);
+        return this;
+    }
+
+    /**
+     * shorthand for two col configuration
+     *
+     * @param position       TOP or ASIDE
+     * @param minWidthTwoCol good value 21em
+     * @return itself for fluent writing
+     */
+    public VFormLayout withResponsiveStepsTwoCols(ResponsiveStep.LabelsPosition position, String minWidthTwoCol) {
+        return withResponsiveSteps(new ResponsiveStep("0", 1, position),
+                new ResponsiveStep(minWidthTwoCol, 2, position));
+    }
+
+    /**
+     * shorthand for three col configuration
+     *
+     * @param position         TOP or ASIDE
+     * @param minWidthTwoCol   good value 21em
+     * @param minWidthThreeCol good value 14em
+     * @return itself for fluent writing
+     */
+    public VFormLayout withResponsiveStepsThreeCols(ResponsiveStep.LabelsPosition position, String minWidthTwoCol, String minWidthThreeCol) {
+        return withResponsiveSteps(new ResponsiveStep("0", 1, position),
+                new ResponsiveStep(minWidthTwoCol, 2, position),
+                new ResponsiveStep(minWidthThreeCol, 3, position));
     }
 }
