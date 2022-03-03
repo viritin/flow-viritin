810cae9644304f7eb30376ba7f6b0faf7031ff85
Matti Tahvonen <matti@vaadin.com>
2022-03-03 12:15:11 +0200

    Cherry picked EnumSelect improvements ti v23 branch

--- src/main/java/org/vaadin/firitin/fields/EnumSelect.java
+++ src/main/java/org/vaadin/firitin/fields/EnumSelect.java
@@ -2,21 +2,27 @@ package org.vaadin.firitin.fields;
 
 import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.component.combobox.ComboBox;
-import org.vaadin.firitin.components.combobox.VComboBox;
-import org.vaadin.firitin.fluency.ui.*;
+import java.util.Arrays;
+import org.vaadin.firitin.fluency.ui.FluentComponent;
+import org.vaadin.firitin.fluency.ui.FluentFocusable;
+import org.vaadin.firitin.fluency.ui.FluentHasSize;
+import org.vaadin.firitin.fluency.ui.FluentHasStyle;
+import org.vaadin.firitin.fluency.ui.FluentHasValidation;
+import org.vaadin.firitin.fluency.ui.FluentHasValueAndElement;
 import org.vaadin.firitin.fluency.ui.internal.FluentHasAutofocus;
 import org.vaadin.firitin.fluency.ui.internal.FluentHasLabel;
 
-import java.util.Arrays;
+public class EnumSelect<T> extends ComboBox<T> implements FluentHasSize<EnumSelect<T>>,
+    FluentHasValidation<EnumSelect<T>>, FluentFocusable<ComboBox<T>, EnumSelect<T>>,
+    FluentComponent<EnumSelect<T>>, FluentHasLabel<EnumSelect<T>>, FluentHasAutofocus<EnumSelect<T>>,
+    FluentHasStyle<EnumSelect<T>>, FluentHasValueAndElement<EnumSelect<T>, AbstractField.ComponentValueChangeEvent<ComboBox<T>, T>, T> {
 
-public class EnumSelect<T extends Enum> extends VComboBox<T> implements FluentHasSize<VComboBox<T>>, FluentHasValidation<VComboBox<T>>, FluentFocusable<ComboBox<T>, VComboBox<T>>, FluentComponent<VComboBox<T>>, FluentHasLabel<VComboBox<T>>, FluentHasAutofocus<VComboBox<T>>, FluentHasStyle<VComboBox<T>>, FluentHasValueAndElement<VComboBox<T>, AbstractField.ComponentValueChangeEvent<ComboBox<T>, T>, T>, FluentHasHelper<VComboBox<T>> {
+  public EnumSelect(Class<T> enumClass) {
+    this(null, enumClass);
+  }
 
-    public EnumSelect(Class<T> enumClass) {
-        this(null, enumClass);
-    }
-
-    public EnumSelect(String caption, Class<T> enumClass) {
-        super(caption, Arrays.asList(enumClass.getEnumConstants()));
-    }
+  public EnumSelect(String caption, Class<T> enumClass) {
+    super(caption, Arrays.asList(enumClass.getEnumConstants()));
+  }
 
 }
