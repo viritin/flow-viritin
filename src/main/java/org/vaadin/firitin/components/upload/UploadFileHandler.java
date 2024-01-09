868b0a7f178add965b345b1122a5eb3d6a1d6fef
Matti Tahvonen <matti@vaadin.com>
2024-01-09 17:14:46 +0200

    implementing some old VUpload API for better backwards compatibility

--- src/main/java/org/vaadin/firitin/components/upload/UploadFileHandler.java
+++ src/main/java/org/vaadin/firitin/components/upload/UploadFileHandler.java
@@ -25,12 +25,17 @@ import com.vaadin.flow.component.EventData;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.shared.SlotUtils;
+import com.vaadin.flow.component.upload.UploadI18N;
+import com.vaadin.flow.function.SerializableConsumer;
+import com.vaadin.flow.internal.JsonSerializer;
 import com.vaadin.flow.server.Command;
 import com.vaadin.flow.server.RequestHandler;
 import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.server.VaadinResponse;
 import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.shared.Registration;
+import elemental.json.JsonObject;
+import elemental.json.JsonType;
 import org.vaadin.firitin.fluency.ui.FluentComponent;
 import org.vaadin.firitin.fluency.ui.FluentHasSize;
 import org.vaadin.firitin.fluency.ui.FluentHasStyle;
@@ -39,6 +44,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -55,6 +61,8 @@ import java.util.UUID;
  */
 @Tag("vaadin-upload")
 public class UploadFileHandler extends Component implements FluentComponent<UploadFileHandler>, FluentHasStyle<UploadFileHandler>, FluentHasSize<UploadFileHandler> {
+
+    private UploadI18N i18n;
 
     @FunctionalInterface
     public interface FileHandler {
@@ -199,6 +207,12 @@ public class UploadFileHandler extends Component implements FluentComponent<Uplo
 
         this.ui = attachEvent.getUI();
         super.onAttach(attachEvent);
+
+        // Element state is not persisted across attach/detach
+        if (this.i18n != null) {
+            setI18nWithJS();
+        }
+
     }
 
     @Override
@@ -301,6 +315,127 @@ public class UploadFileHandler extends Component implements FluentComponent<Uplo
     public UploadFileHandler withUploadButton(Component button) {
         setUploadButton(button);
         return this;
+    }
+
+    /**
+     * Specify the types of files that the server accepts. Syntax: a MIME type
+     * pattern (wildcards are allowed) or file extensions. Notice that MIME
+     * types are widely supported, while file extensions are only implemented in
+     * certain browsers, so it should be avoided.
+     * <p>
+     * Example: <code>"video/*","image/tiff"</code> or
+     * <code>".pdf","audio/mp3"</code>
+     *
+     * @param acceptedFileTypes
+     *            the allowed file types to be uploaded, or <code>null</code> to
+     *            clear any restrictions
+     */
+    public void setAcceptedFileTypes(String... acceptedFileTypes) {
+        String accepted = "";
+        if (acceptedFileTypes != null) {
+            accepted = String.join(",", acceptedFileTypes);
+        }
+        getElement().setProperty("accept", accepted);
+    }
+
+    public UploadFileHandler withAcceptedFileTypes(String... acceptedFileTypes) {
+        setAcceptedFileTypes(acceptedFileTypes);
+        return this;
+    }
+
+    /**
+     * Set the component to show as a message to the user to drop files in the
+     * upload component. Despite of the name, the label can be any component.
+     *
+     * @param label
+     *            the label to show for the users when it's possible drop files,
+     *            or <code>null</code> to reset to the default label
+     */
+    public void setDropLabel(Component label) {
+        SlotUtils.setSlot(this, "drop-label", label);
+    }
+
+    public UploadFileHandler withDropLabel(Component label) {
+        setDropLabel(label);
+        return this;
+    }
+
+    /**
+     * Set the component to show as the drop label icon. The icon is visible
+     * when the user can drop files to this upload component. Despite of the
+     * name, the drop label icon can be any component.
+     *
+     * @param icon
+     *            the label icon to show for the users when it's possible to
+     *            drop files, or <code>null</code> to reset to the default icon
+     */
+    public void setDropLabelIcon(Component icon) {
+        SlotUtils.setSlot(this, "drop-label-icon", icon);
+    }
+
+    public UploadFileHandler withDropLabelIcon(Component icon) {
+        setDropLabelIcon(icon);
+        return this;
+    }
+
+    /**
+     * Set the internationalization properties for this component.
+     *
+     * @param i18n
+     *            the internationalized properties, not <code>null</code>
+     */
+    public void setI18n(UploadI18N i18n) {
+        Objects.requireNonNull(i18n,
+                "The I18N properties object should not be null");
+        this.i18n = i18n;
+
+        runBeforeClientResponse(ui -> {
+            if (i18n == this.i18n) {
+                setI18nWithJS();
+            }
+        });
+    }
+
+    private void setI18nWithJS() {
+        JsonObject i18nJson = (JsonObject) JsonSerializer.toJson(this.i18n);
+
+        // Remove null values so that we don't overwrite existing WC
+        // translations with empty ones
+        deeplyRemoveNullValuesFromJsonObject(i18nJson);
+
+        // Assign new I18N object to WC, by deeply merging the existing
+        // WC I18N, and the values from the new UploadI18N instance,
+        // into an empty object
+        getElement().executeJs(
+                "const dropFiles = Object.assign({}, this.i18n.dropFiles, $0.dropFiles);"
+                        + "const addFiles = Object.assign({}, this.i18n.addFiles, $0.addFiles);"
+                        + "const error = Object.assign({}, this.i18n.error, $0.error);"
+                        + "const uploadingStatus = Object.assign({}, this.i18n.uploading.status, $0.uploading && $0.uploading.status);"
+                        + "const uploadingRemainingTime = Object.assign({}, this.i18n.uploading.remainingTime, $0.uploading && $0.uploading.remainingTime);"
+                        + "const uploadingError = Object.assign({}, this.i18n.uploading.error, $0.uploading && $0.uploading.error);"
+                        + "const uploading = {status: uploadingStatus,"
+                        + "  remainingTime: uploadingRemainingTime,"
+                        + "  error: uploadingError};"
+                        + "const units = $0.units || this.i18n.units;"
+                        + "this.i18n = Object.assign({}, this.i18n, $0, {"
+                        + "  addFiles: addFiles,  dropFiles: dropFiles,"
+                        + "  uploading: uploading, units: units});",
+                i18nJson);
+    }
+
+    private void deeplyRemoveNullValuesFromJsonObject(JsonObject jsonObject) {
+        for (String key : jsonObject.keys()) {
+            if (jsonObject.get(key).getType() == JsonType.OBJECT) {
+                deeplyRemoveNullValuesFromJsonObject(jsonObject.get(key));
+            } else if (jsonObject.get(key).getType() == JsonType.NULL) {
+                jsonObject.remove(key);
+            }
+        }
+    }
+
+    void runBeforeClientResponse(SerializableConsumer<UI> command) {
+        getElement().getNode().runWhenAttached(ui -> ui
+                .beforeClientResponse(this, context -> command.accept(ui)));
     }
 
 
--- src/test/java/org/vaadin/firitin/UploadFileHandlerExample.java
+++ src/test/java/org/vaadin/firitin/UploadFileHandlerExample.java
@@ -23,6 +23,7 @@ import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.component.upload.UploadI18N;
 import com.vaadin.flow.router.Route;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -136,7 +137,13 @@ public class UploadFileHandlerExample extends VerticalLayout {
                     } catch (IOException ex) {
                         Logger.getLogger(UploadFileHandlerExample.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                });
+                })
+                .withAcceptedFileTypes("text/csv")
+                .withDropLabelIcon(VaadinIcon.DROP.create())
+                ;
+
+        csvFileHandler.setI18n(new UploadExamplesI18N());
+        csvFileHandler.getStyle().setBorder("2px dotted green");
 
         add(
                 new Paragraph("Count lines"),
@@ -146,6 +153,43 @@ public class UploadFileHandlerExample extends VerticalLayout {
                 multiUploadFileHandler,
                 new Paragraph("Display CSV file"), csvFileHandler, grid);
 
+    }
+
+
+    /**
+     * Provides a default I18N configuration for the Upload examples
+     *
+     * At the moment the Upload component requires a fully configured I18N instance,
+     * even for use-cases where you only want to change individual texts.
+     *
+     * This I18N configuration is an adaption of the web components I18N defaults
+     * and can be used as a basis for customizing individual texts.
+     */
+    public static class UploadExamplesI18N extends UploadI18N {
+        public UploadExamplesI18N() {
+            setDropFiles(new DropFiles().setOne("-> HERE <--")
+                    .setMany("-> HERE <--"));
+            setAddFiles(new AddFiles().setOne("Upload File...")
+                    .setMany("Upload Files..."));
+            setError(new Error().setTooManyFiles("Too Many Files.")
+                    .setFileIsTooBig("File is Too Big.")
+                    .setIncorrectFileType("Incorrect File Type."));
+            setUploading(new Uploading()
+                    .setStatus(new Uploading.Status().setConnecting("Connecting...")
+                            .setStalled("Stalled")
+                            .setProcessing("Processing File...").setHeld("Queued"))
+                    .setRemainingTime(new Uploading.RemainingTime()
+                            .setPrefix("remaining time: ")
+                            .setUnknown("unknown remaining time"))
+                    .setError(new Uploading.Error()
+                            .setServerUnavailable(
+                                    "Upload failed, please try again later")
+                            .setUnexpectedServerError(
+                                    "Upload failed due to server error")
+                            .setForbidden("Upload forbidden")));
+            setUnits(new Units().setSize(Arrays.asList("B", "kB", "MB", "GB", "TB",
+                    "PB", "EB", "ZB", "YB")));
+        }
     }
 
 }
