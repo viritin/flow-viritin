8fa3363a15d680b98232f45849ee699dbd81d88e
Matti Tahvonen <matti@vaadin.com>
2024-03-09 00:43:42 +0200

    Make the field behave properly (report to server lazily)

--- src/main/java/org/vaadin/firitin/fields/CommaSeparatedStringField.java
+++ src/main/java/org/vaadin/firitin/fields/CommaSeparatedStringField.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.component.AbstractCompositeField;
 import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.textfield.TextField;
+import org.vaadin.firitin.components.textfield.VTextField;
+import org.vaadin.firitin.fluency.ui.FluentHasSize;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -12,7 +14,7 @@ import java.util.Set;
 /**
  * A field to edit a set of String using one simple text field. Strings are separated using a comma and optional white-space.
  */
-public class CommaSeparatedStringField extends AbstractCompositeField<TextField, CommaSeparatedStringField, Set<String>> implements HasSize {
+public class CommaSeparatedStringField extends AbstractCompositeField<VTextField, CommaSeparatedStringField, Set<String>> implements FluentHasSize<CommaSeparatedStringField> {
 
     private Set<String> oldValue;
 
