17e47d9a2c3199ba3bc6f3a0660de546efc4055d
Matti Tahvonen <matti@vaadin.com>
2024-03-13 19:17:05 +0200

    A component (ab)using vaadin-message to display markdown input
    For example by ChatGPT

---
+++ src/main/java/org/vaadin/firitin/components/messagelist/MarkdownMessage.java
@@ -0,0 +1,143 @@
+package org.vaadin.firitin.components.messagelist;
+
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ScrollOptions;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.server.Command;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Tag("vaadin-message")
+public class MarkdownMessage extends Component {
+
+    private static HtmlRenderer renderer;
+    private static Parser parser;
+    private UI ui;
+    private String markdown;
+    private String previousHtml;
+
+    private Element content = new Element("div");
+
+    public MarkdownMessage(String name, LocalDateTime timestamp, Integer userColorIndex) {
+        setUserColorIndex(userColorIndex);
+        getElement().setProperty("userName", name);
+        getElement().setProperty("time", timestamp.format(DateTimeFormatter.ofPattern("YYYY-MM-DD hh:mm")));
+        getElement().appendChild(content);
+    }
+
+    public MarkdownMessage(String name, Integer userColorIndex) {
+        this(name, LocalDateTime.now(), userColorIndex);
+    }
+
+    public MarkdownMessage(String initialContent, String name, Integer userColorIndex) {
+        this(name, LocalDateTime.now(), userColorIndex);
+        appendMarkdown(initialContent);
+    }
+
+    public MarkdownMessage(String markdown, String name, LocalDateTime timestamp) {
+        this(name, timestamp, 0);
+        String html = getMdRenderer().render(getMdParser().parse(markdown));
+        appendHtml(html);
+        this.markdown = markdown;
+        this.previousHtml = html;
+    }
+
+    public void setUserColorIndex(int index) {
+        getElement().setProperty("userColorIndex", index);
+    }
+
+    private void appendHtml(String html) {
+        getElement().executeJs("""
+                if(this.curHtml) {
+                    this.curHtml = this.curHtml + $0;
+                } else {
+                    this.curHtml = $0;
+                }
+                $1.innerHTML = this.curHtml;
+                """, html, content);
+    }
+    private void appendHtml(String html, int replaceFrom) {
+        getElement().executeJs("""
+                this.curHtml = this.curHtml.substring(0, $2) + $0; 
+                $1.innerHTML = this.curHtml;
+                """, html, content, replaceFrom);
+    }
+
+    protected HtmlRenderer getMdRenderer() {
+        if (renderer == null) {
+            renderer = HtmlRenderer.builder().build();
+        }
+        return renderer;
+    }
+
+    protected Parser getMdParser() {
+        if (parser == null) {
+            MutableDataSet options = new MutableDataSet();
+            parser = Parser.builder(options).build();
+        }
+        return parser;
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        this.ui = attachEvent.getUI();
+    }
+
+    public UI getUi() {
+        if(ui == null) {
+            // fallback, but not 100% thread safe, thanks to all involved :-)
+            ui = ui.getUI().orElseGet(() -> UI.getCurrent());
+        }
+        return ui;
+    }
+    public void appendMarkdown(String markdownSnippet) {
+        appendMarkdown(markdownSnippet, false);
+    }
+    public void appendMarkdownAsync(String markdownSnippet) {
+        appendMarkdown(markdownSnippet, true);
+    }
+
+    protected void appendMarkdown(String markdownSnippet, boolean uiAccess) {
+        if(markdown == null) {
+            markdown = markdownSnippet;
+        } else {
+            markdown += markdownSnippet;
+        }
+        String html = getMdRenderer().render(getMdParser().parse(markdown));
+        Command c;
+        if(previousHtml == null) {
+            c = () -> appendHtml(html);
+        } else {
+            String commonPrefix = StringUtils.getCommonPrefix(html, previousHtml);
+            int startOfNew = commonPrefix.length();
+            String newPart = html.substring(startOfNew);
+            c  = () -> {
+                appendHtml(newPart, startOfNew);
+                content.scrollIntoView();
+            };
+        }
+        previousHtml = html;
+        if(uiAccess) {
+            getUi().access(c);
+        } else {
+            c.execute();
+        }
+
+    }
+
+    public void appendText(String text) {
+        if(!text.endsWith("\n")) {
+            text = text + "\n";
+        }
+        appendMarkdown(text);
+    }
+}
---
+++ src/test/java/org/vaadin/firitin/MessageListView.java
@@ -0,0 +1,78 @@
+package org.vaadin.firitin;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.router.Route;
+import org.vaadin.firitin.components.messagelist.MarkdownMessage;
+import org.vaadin.firitin.components.orderedlayout.VVerticalLayout;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Route
+public class MessageListView extends VVerticalLayout {
+
+    static ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private ScheduledFuture<?> scheduledFuture;
+
+    public MessageListView() {
+        MarkdownMessage message = new MarkdownMessage("Hello!\n","John", LocalDateTime.now());
+        add(message);
+
+        add(
+                new Button("Append markdown", event -> {
+                    message.appendMarkdown("### Appended \n Some more text\n");
+                }),
+                new Button("Append text", event -> {
+                    message.appendText("<h3>Appended</h3><p>Some more text</p>");
+                }),
+                new Button("Append Markdown", event -> {
+                    message.appendMarkdown("""
+                            # Appended markdown title
+                                                
+                             * List 1
+                             * List 2
+                             
+                             Some more text
+                            """);
+                }),
+
+                new Button("Subscribe to streaming", event -> {
+                    String md = """
+                    # Appended markdown title
+                    
+                    So this is [an anchor](https://github.com/mstahv) to resource in the GitHub.
+                    
+                     * List 1 is pretty long one
+                     * List 2
+                    
+                    Some more text...
+                    """;
+                    Random r = new Random(0);
+                    AtomicInteger sent = new AtomicInteger();
+
+                    scheduledFuture = executorService.scheduleAtFixedRate(() -> {
+                        int charsToSend = r.nextInt(5, 10);
+                        int start = sent.get();
+                        int end = start + charsToSend;
+                        if(start + charsToSend > md.length()) {
+                            end = md.length();
+                        }
+                        String token = md.substring(start, end);
+
+                        message.appendMarkdownAsync(token);
+
+                        int currentlySent = sent.addAndGet(charsToSend);
+                        if(currentlySent > md.length()) {
+                            scheduledFuture.cancel(true);
+                        }
+                    }, 500, 500, TimeUnit.MILLISECONDS);
+
+                }));
+
+    }
+}
