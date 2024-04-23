5b8a80100a1fe10c2fe78cba8080efa8fab77d51
Matti Tahvonen <matti@vaadin.com>
2024-04-23 11:47:02 +0300

    DurationField
    A field impementation to edit java.time.Duration data type

---
+++ src/main/java/org/vaadin/firitin/components/DurationField.java
@@ -0,0 +1,130 @@
+package org.vaadin.firitin.components;
+
+import com.vaadin.flow.component.HasPlaceholder;
+import com.vaadin.flow.component.customfield.CustomField;
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.vaadin.firitin.components.textfield.VTextField;
+
+import java.time.Duration;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+
+public class DurationField extends CustomField<Duration> implements HasPlaceholder {
+    VTextField durationInput = new VTextField()
+            .withPlaceholder("hh:mm:ss");
+
+    public DurationField() {
+        this(null);
+    }
+
+    public DurationField(String label) {
+        setLabel(label);
+        durationInput.setWidth("8em");
+        durationInput.addBlurListener(e -> {
+            // Normalize formatting on blur
+            String str = durationInput.getValue();
+            if (!durationInput.isInvalid()) {
+                Duration duration = parseInput(durationInput.getValue());
+                durationInput.setValue(formatToPresentation(duration));
+            }
+        });
+        add(durationInput);
+        setTooltipText("""
+            Default formatting for duration: hh:mm:ss.
+            For quick input, if no separator typed in, until 100, interpreted an hour.
+            Then until 10000, hhmm.
+        """);
+    }
+
+    protected String formatToPresentation(Duration d) {
+        return defaultFormatToPresentation(d);
+    }
+
+    public static final String defaultFormatToPresentation(Duration d) {
+        if (d == null) {
+            return "";
+        }
+        long secs = d.get(ChronoUnit.SECONDS);
+        boolean hasSecs = (secs % 60) != 0;
+        if (hasSecs) {
+            return DurationFormatUtils.formatDuration(secs * 1000, "H:mm:ss", true);
+        } else {
+            return DurationFormatUtils.formatDuration(secs * 1000, "H:mm", true);
+        }
+    }
+
+    protected Duration parseInput(String value) {
+        return parseInputDefault(value);
+    }
+
+    static final Duration parseInputDefault(String value) {
+        try {
+            // First try with ISO-8601: PnDTnHnMn.nS
+            return Duration.parse(value);
+        } catch (DateTimeParseException e) {
+            if (value.contains(":")) {
+                value = value.replaceFirst(":", "H");
+                if (value.contains(":")) {
+                    value = value.replaceFirst(":", "M") + "S";
+                } else {
+                    value += "M";
+                }
+                value = "PT" + value;
+                try {
+                    return Duration.parse(value);
+                } catch (DateTimeParseException exception) {
+                    return null;
+                }
+            } else {
+                boolean numbersOnly = value.matches("[0-9]+");
+                if (numbersOnly) {
+                    try {
+                        if (value.length() < 3) {
+                            int hours = Integer.parseInt(value);
+                            return Duration.ofHours(hours);
+                        } else {
+                            int firstMinute = value.length() - 2;
+                            int minutes = Integer.parseInt(value.substring(firstMinute));
+                            int hours = Integer.parseInt(value.substring(0, firstMinute));
+                            return Duration.ofMinutes(minutes + hours * 60);
+                        }
+                    } catch (NumberFormatException numberFormatException) {
+                        return null;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected Duration generateModelValue() {
+        String value = durationInput.getValue();
+        Duration d = parseInput(value);
+        durationInput.setInvalid(d == null && !value.isEmpty());
+        return d;
+    }
+
+    @Override
+    protected void setPresentationValue(Duration newPresentationValue) {
+        String presentation = formatToPresentation(newPresentationValue);
+        durationInput.setValue(presentation);
+    }
+
+    @Override
+    public void setInvalid(boolean invalid) {
+        super.setInvalid(invalid);
+        // proxy also to the underlaying TextField (red background if invalid)
+        durationInput.setInvalid(invalid);
+    }
+
+    // HasPlaceholder implementation, proxy to underlying TextField
+    public void setPlaceholder(String placeholder) {
+        durationInput.setPlaceholder(placeholder);
+    }
+
+    @Override
+    public String getPlaceholder() {
+        return durationInput.getPlaceholder();
+    }
+}
---
+++ src/test/java/org/vaadin/firitin/DurationView.java
@@ -0,0 +1,76 @@
+package org.vaadin.firitin;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import jakarta.validation.constraints.NotNull;
+import org.vaadin.firitin.components.DurationField;
+import org.vaadin.firitin.components.button.VButton;
+import org.vaadin.firitin.components.orderedlayout.VVerticalLayout;
+import org.vaadin.firitin.form.FormBinder;
+
+import java.time.Duration;
+
+@Route
+public class DurationView extends VVerticalLayout {
+
+    public record Dto(String text, @NotNull Duration duration) {}
+
+    DurationField duration = new DurationField("Input duration here");
+    TextField text = new TextField("Basic text");
+
+    public DurationView() {
+
+        duration.setManualValidation(true);
+
+
+        duration.setLabel("Label for field");
+        duration.setRequiredIndicatorVisible(true);
+        duration.setTooltipText("This showed when hovered with mouse.");
+        duration.setInvalid(true);
+        duration.setErrorMessage("This makes the field red and error message visible.");
+        duration.setHelperText("This is so called helper text. Kind of like tooltip but always visible.");
+
+        add(duration, text);
+        if(true) {
+            return;
+        }
+
+        add("Duration (Duration.toString()):");
+        var value = new Paragraph();
+        add(value);
+
+        FormBinder<Dto> binder = new FormBinder<>(Dto.class, this);
+
+        duration.addValueChangeListener(e -> {
+            Duration d = e.getValue();
+            value.setText(""+d);
+        });
+
+        add(new VButton("Binder dto.toString()", () -> Notification.show(binder.getValue().toString())));
+
+        add(new Button("Set 24h", e-> {
+            duration.setValue(Duration.ofDays(1));
+        }));
+
+        add(new Button("Set 1s", e-> {
+            duration.setValue(Duration.ofSeconds(1));
+        }));
+
+        add(new Button("Set 1m", e-> {
+            duration.setValue(Duration.ofMinutes(1));
+        }));
+
+        add(new Button("Set 10m", e-> {
+            duration.setValue(Duration.ofMinutes(10));
+        }));
+        add(new Button("Set 61m", e-> {
+            duration.setValue(Duration.ofMinutes(61));
+        }));
+
+    }
+
+
+}
