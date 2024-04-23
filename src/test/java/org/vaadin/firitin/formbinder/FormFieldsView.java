8217f65a54a36cd033445b3712ff9d51832399b1
Matti Tahvonen <matti@vaadin.com>
2024-04-23 17:11:31 +0300

    improved example

--- src/test/java/org/vaadin/firitin/formbinder/FormFieldsView.java
+++ src/test/java/org/vaadin/firitin/formbinder/FormFieldsView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.vaadin.firitin.appframework.MenuItem;
+import org.vaadin.firitin.fields.CommaSeparatedStringField;
 import org.vaadin.firitin.fields.DurationField;
 import org.vaadin.firitin.components.RichText;
 import org.vaadin.firitin.fields.ElementCollectionField;
@@ -15,7 +16,10 @@ import org.vaadin.firitin.form.BeanValidationForm;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Route
 @MenuItem(icon = VaadinIcon.EDIT)
@@ -29,17 +33,18 @@ public class FormFieldsView extends BeanValidationForm<FormFieldsView.MyRecord> 
     public record Element(String name, Integer value, MyEnum myEnum) {
     }
 
-    public record MyRecord(MyEnum myEnum, Duration duration, List<Element> children) {
+    public record MyRecord(MyEnum myEnum, Duration duration, List<Element> children, Set<String> strings) {
     }
 
     private DurationField duration = new DurationField("Input duration here");
     private EnumSelect<MyEnum> myEnum = new EnumSelect<>(MyEnum.class)
             .withLabel("Select enum value");
     private ElementCollectionField<Element> children = new ElementCollectionField<>(Element.class);
+    private CommaSeparatedStringField strings = new CommaSeparatedStringField("Strings separated with commas");
 
     public FormFieldsView() {
         super(MyRecord.class);
-        setEntity(new MyRecord(MyEnum.ONE, Duration.ofHours(1).plusSeconds(30), new ArrayList<>()));
+        setEntity(new MyRecord(MyEnum.ONE, Duration.ofHours(1).plusSeconds(30), new ArrayList<>(), Set.of("one", "two", "three")));
         setSavedHandler(mydata -> {
             Notification.show("Saved: " + mydata);
         });
@@ -47,7 +52,7 @@ public class FormFieldsView extends BeanValidationForm<FormFieldsView.MyRecord> 
 
     @Override
     protected List<Component> getFormComponents() {
-        return Arrays.asList(duration, myEnum, children);
+        return Arrays.asList(duration, myEnum, children, strings);
     }
 
     @Override
@@ -56,7 +61,7 @@ public class FormFieldsView extends BeanValidationForm<FormFieldsView.MyRecord> 
         layout.addComponentAsFirst(new RichText().withMarkDown("""
                         # Generic form fields from the Viritin add-on
                                         
-                        This view shows various generic form fields and their usage. 
+                        This view shows some of the generic form fields and their usage. 
                         The form is implemented BeanValidationForm from Viritin add-on,
                         but the fields themselves are compatible with the basic Binder from 
                         Vaadin core as well.
