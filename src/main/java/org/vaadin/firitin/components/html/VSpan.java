5056bc0d29c69b0bbc2e128a97e54ea45f06172d
Matti Tahvonen <matti@vaadin.com>
2024-07-26 15:26:23 +0300

    Replaced backwards incompatible [3ee94e3] with other helper

--- src/main/java/org/vaadin/firitin/components/html/VSpan.java
+++ src/main/java/org/vaadin/firitin/components/html/VSpan.java
@@ -10,15 +10,15 @@ public class VSpan extends Span implements FluentHtmlContainer<VSpan>, FluentCli
         super();
     }
 
-    public VSpan(Object text) {
-        super(text.toString());
-    }
-
     public VSpan(String text) {
         super(text);
     }
 
     public VSpan(Component... components) {
         super(components);
+    }
+
+    public static VSpan of(Object text) {
+        return new VSpan(text.toString());
     }
 }
