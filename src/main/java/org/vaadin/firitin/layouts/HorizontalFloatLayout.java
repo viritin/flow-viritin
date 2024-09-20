e16d7fb76caf0ba5fa07c97cc687ad1e9831a009
Matti Tahvonen <matti@vaadin.com>
2024-09-20 22:39:18 +0300

    Added constructor

--- src/main/java/org/vaadin/firitin/layouts/HorizontalFloatLayout.java
+++ src/main/java/org/vaadin/firitin/layouts/HorizontalFloatLayout.java
@@ -1,5 +1,6 @@
 package org.vaadin.firitin.layouts;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.vaadin.firitin.components.orderedlayout.VHorizontalLayout;
 
@@ -7,5 +8,10 @@ public class HorizontalFloatLayout extends VHorizontalLayout {
     public HorizontalFloatLayout() {
         setDefaultVerticalComponentAlignment(Alignment.BASELINE);
         addClassName(LumoUtility.FlexWrap.WRAP);
+    }
+
+    public HorizontalFloatLayout(Component... components) {
+        this();
+        add(components);
     }
 }
