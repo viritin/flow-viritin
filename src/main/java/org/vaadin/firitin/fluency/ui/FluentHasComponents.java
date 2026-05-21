ab30d0b192b95088303b74837374988411a60772
Matti Tahvonen <matti@vaadin.com>
2026-05-21 11:13:16 +0300

    Component tree helpers
    Framework don't provide any proper solution to get the full component tree. This implements it using the "internal" StateNode feature.

--- src/main/java/org/vaadin/firitin/fluency/ui/FluentHasComponents.java
+++ src/main/java/org/vaadin/firitin/fluency/ui/FluentHasComponents.java
@@ -2,6 +2,9 @@ package org.vaadin.firitin.fluency.ui;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasComponents;
+import org.vaadin.firitin.util.ComponentTree;
+
+import java.util.stream.Stream;
 
 @SuppressWarnings("unchecked")
 public interface FluentHasComponents<S extends FluentHasComponents<S>> extends HasComponents {
@@ -20,4 +23,21 @@ public interface FluentHasComponents<S extends FluentHasComponents<S>> extends H
         addComponentAtIndex(index, component);
         return (S) this;
     }
+
+    /**
+     * @return children via backing "StateNode" structure, so that all components are listed,
+     * regardless of the component implementation.
+     */
+    default Stream<Component> children() {
+        return ComponentTree.children((Component) this);
+    }
+
+    /**
+     * @return All descendants (via backing StateNode structure, so that all components are truly listed) in
+     * pre-order.
+     */
+    default Stream<Component> descendants() {
+        return ComponentTree.descendants((Component) this);
+    }
+
 }
--- src/test/java/org/vaadin/firitin/appframework/MySecondView.java
+++ src/test/java/org/vaadin/firitin/appframework/MySecondView.java
@@ -1,7 +1,19 @@
 package org.vaadin.firitin.appframework;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Paragraph;
+
+import com.vaadin.flow.component.notification.Notification;
+import org.vaadin.firitin.components.Tree;
+import org.vaadin.firitin.fluency.ui.FluentHasComponents;
+import org.vaadin.firitin.util.ComponentTree;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 // Same order as with MyFirstView.java, but title (derived from classname) after it alphabetically
 @MenuItem(order = MenuItem.BEGINNING)
@@ -14,5 +26,46 @@ public class MySecondView extends MyAbstractView {
             item.setEnabled(!item.isEnabled());
         }));
 
+        add(new Button("Component tree with HasComponents", e-> {
+            new ComponentTreeWithHasComponents(UI.getCurrent());
+        }));
+
+        add(new Button("Component tree with Element API", e-> {
+            new ComponentTreeWithStateNode(UI.getCurrent());
+        }));
+        add(new Button("Direct children of this via VHasComponent.children", e-> {
+            String children = children().map(component -> component.getClass().getSimpleName()).collect(Collectors.joining(","));
+            Notification.show("Direct children: " + children);
+        }));
+
+    }
+
+    public class ComponentTreeWithHasComponents extends Dialog {
+        public ComponentTreeWithHasComponents(UI ui) {
+            super();
+            setHeaderTitle("Component Tree (with HasComponents):");
+            add(componentTree(ui,
+                    c -> c instanceof HasComponents hc ? hc.getChildren().toList() : List.of()));
+            open();
+        }
+    }
+
+    public class ComponentTreeWithStateNode extends Dialog {
+        public ComponentTreeWithStateNode(UI ui) {
+            super();
+            setHeaderTitle("Component Tree (with Element API):");
+            add(componentTree(ui, c -> ComponentTree.children(c).toList()));
+            open();
+        }
+    }
+
+
+    private static Tree<Component> componentTree(Component root,
+            Tree.ChildrenProvider<Component> childrenProvider) {
+        Tree<Component> tree = new Tree<>();
+        tree.setItemLabelGenerator(c -> c.getClass().getSimpleName());
+        tree.setItems(root, childrenProvider);
+        tree.showChildrenRecursively(root);
+        return tree;
     }
 }
